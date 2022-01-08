c94e7c12f79e0c0ef76603e2d16ba69bb33617b0
Lee Sutton <leesutton1@gmail.com>
2022-01-08 09:51:14 -0800

    added python host prog

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,6 +41,8 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
+vim.g["python3_host_prog"] = "/Users/leesutton/mambaforge/bin/python3"
+
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
