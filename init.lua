b88881224ef4e95fb458b7251a83420eb3c7313c
Lee Sutton <lee.sutton@keela.com>
2025-06-24 10:31:03 -0700

    feat: update fold expression

--- init.lua
+++ init.lua
@@ -4,3 +4,6 @@ require 'keymaps'
 vim.cmd 'let g:test#python#runner = "pytest"'
 vim.cmd 'let g:test#javascript#runner = "vitest"'
 vim.cmd 'set nowrap'
+
+vim.cmd 'set foldmethod=expr'
+vim.cmd 'set foldexpr=nvim_treesitter#foldexpr()'
